2d7ddc497a5bde1741c903265eb76fc41ebe9722
Kapil Gupta <20682137+kapsay@users.noreply.github.com>
2021-11-05 22:26:18 +0000

    Change#4

--- s3c-scanner/scanner.sh
+++ s3c-scanner/scanner.sh
@@ -3,7 +3,6 @@
 # -------------------------------
 image_name="gcr.io/$PROJECT_ID/$CLOUD_RUN_APP_NAME"
 echo "----- $image_name"
-#image_name="gcr.io/ah-crm-common-gke-np-105/state-manager"
 digest_value=$(gcloud beta container images list-tags $image_name --format="table[no-heading](digest.slice(0:256).join(''))" --limit=1)
 echo "----- $digest_value"
 vuln_counter=$(gcloud beta container images describe $image_name@$digest_value --show-package-vulnerability --format="table[no-heading](package_vulnerability_summary.vulnerabilities.CRITICAL[].vulnerability.cvssScore)")
@@ -13,13 +12,8 @@ echo "* ------------------------------------------------------------------------
 if [[ $vuln_counter == "" ]]
 then
     echo "Continue - No Critical vulnerability found"
-#    echo "gcloud beta container binauthz attestations sign-and-create --project $PROJECT_ID --artifact-url $image_name@$digest_value --attestor $BA_VULNERABILITY_ATTESTOR --attestor-project $PROJECT_ID --keyversion 1 --keyversion-key $BA_KEY --keyversion-location us-central1 --keyversion-keyring $BA_KEYRING --keyversion-project $PROJECT_ID"
-#    gcloud beta container binauthz attestations sign-and-create --project $PROJECT_ID --artifact-url $image_name@$digest_value --attestor $BA_VULNERABILITY_ATTESTOR --attestor-project $PROJECT_ID --keyversion "1" --keyversion-key $BA_KEY --keyversion-location "us-central1" --keyversion-keyring $BA_KEYRING --keyversion-project $PROJECT_ID
     gcloud beta container binauthz attestations sign-and-create --artifact-url $image_name@$digest_value --attestor=$BA_VULNERABILITY_ATTESTOR --attestor-project=$PROJECT_ID --keyversion="1" --keyversion-key=$BA_KEY --keyversion-location="us-central1" --keyversion-keyring=$BA_KEYRING --keyversion-project=$PROJECT_ID
 
-    #echo "gcloud beta container binauthz attestations sign-and-create --project $PROJECT_ID --artifact-url $image_name@$digest_value --attestor vulnerability-attestor --attestor-project $PROJECT_ID --keyversion 1 --keyversion-key vulnerability-signer --keyversion-location us-central1 --keyversion-keyring binaryauthz --keyversion-project $PROJECT_ID"
-    #gcloud beta container binauthz attestations sign-and-create --project $PROJECT_ID --artifact-url $image_name@$digest_value --attestor "vulnerability-attestor" --attestor-project $PROJECT_ID --keyversion "1" --keyversion-key "vulnerability-signer" --keyversion-location "us-central1" --keyversion-keyring "binaryauthz" --keyversion-project $PROJECT_ID
-    
     echo "Image attestation Binary Authorization is done..."
 else
      
@@ -28,5 +22,3 @@ else
     echo "$result_url"
 fi
 echo "* ---------------------------------------------------------------------------------------------------------------- *"
-
-#DIGEST="sha256:..." # Replace with your value
